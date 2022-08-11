8ac13b99a7cf116c9db20662fc09bf94184bdc5a
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-08-11 16:54:40 -0600

    Final touches

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -20,9 +20,9 @@ const MainPage = () => {
 
   return (
     <div>
-      <div className="heading">
-        <h1>Exiting New Events!</h1>
-        <h2>Please select an event</h2>
+      <div className="title-box">
+        <h1 className="title">Exiting New Events!</h1>
+        <h2 className="subtitle">Please select an event</h2>
       </div>
       <div id="event-list">
         <SemiBtn arrow="prev" disabled={slice < step} func={prevSlice} />
