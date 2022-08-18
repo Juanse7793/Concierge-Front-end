e92202e903eb4e8671960b38a273a23c80966eb8
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-08-18 06:23:28 -0600

    Variably add items on main page

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -7,7 +7,14 @@ import Button from '../components/Button';
 const MainPage = () => {
   const events = useSelector((state) => state.events);
   const [slice, setSlice] = useState(0);
-  const step = 3; // Math.floor((window.innerWidth - (86 * 2)) / 282);
+
+  const [width, setWidth] = useState(window.innerWidth);
+  window.addEventListener('resize', () => { setWidth(window.innerWidth); });
+
+  const wide = width > 768;
+  const step = wide ? Math.floor(((width * 0.8) - (86 * 2)) / 250) : 1;
+  const diameter = (((0.8 * width) - 172) / step) - (2 * 16 * step);
+
   const sliceEvents = events.slice(slice, slice + step);
 
   const prevSlice = () => {
@@ -25,8 +32,8 @@ const MainPage = () => {
         <h1 className="title">EXCITING NEW EVENTS!</h1>
         <h2 className="subtitle">Please select an event to begin:</h2>
       </div>
-      <div className="events-list row">
-        <Button text="◁" disabled={slice < step} func={prevSlice} className="semi prev" />
+      <div className="events-list row" style={{ '--diameter': `min(${diameter}px, 250px)` }}>
+        <Button text="◁" disabled={slice < step} func={prevSlice} className="semi prev green" />
         <div className="events-list row transit">
           <TransitionGroup component={null}>
             {sliceEvents.map((event) => (
@@ -36,7 +43,7 @@ const MainPage = () => {
             ))}
           </TransitionGroup>
         </div>
-        <Button text="▷" disabled={slice > events.length - step} func={nextSlice} className="semi next" />
+        <Button text="▷" disabled={slice > events.length - step} func={nextSlice} className="semi next green" />
       </div>
     </section>
   );
